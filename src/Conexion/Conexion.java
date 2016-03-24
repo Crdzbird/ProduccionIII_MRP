92bf64ed2ba0e53ac166db322dd319c90dce80f2
Ghost-Vegetto <luisalfonsocb83@gmail.com>
2016-03-24 16:48:47 -0600

    Agregada conexion a la BD, y desarrollado interfaz principal

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -9,6 +9,8 @@ import Pojo.Usuario_Conectado;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -18,20 +20,26 @@ import javax.swing.JOptionPane;
 public class Conexion {
 
     static String url = "jdbc:mysql://localhost/SistemaMRP," + Usuario_Conectado.getUser() + "," + Usuario_Conectado.getPass() + ";";
-    
+
     public Connection Coneccion() {
         Connection connection;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            return null;
-        }
-        try {
             connection = DriverManager.getConnection(url);
             return connection;
-        } catch (SQLException io) {
-            JOptionPane.showMessageDialog(null, "Error al realizar coneccion a base de datos", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (ClassNotFoundException | SQLException io) {
+            JOptionPane.showMessageDialog(null, "Error al realizar coneccion a base de datos", "Error de Sistema", JOptionPane.ERROR_MESSAGE);
             return null;
+        }
+    }
+
+    public void CerrarConexion(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Ha sido imposible cerrar la conexion", "Error de Sistema", JOptionPane.ERROR_MESSAGE);
         }
     }
 
--- src/produccioniii_mrp/InterfazPrincipal.java
+++ src/produccioniii_mrp/InterfazPrincipal.java
@@ -5,17 +5,121 @@
  */
 package produccioniii_mrp;
 
+import Pojo.Usuario_Conectado;
+import Validaciones.ImagenFondoDesktop;
+import com.javaswingcomponents.accordion.JSCAccordion;
+import com.javaswingcomponents.accordion.TabOrientation;
+import com.sun.org.apache.xalan.internal.lib.ExsltDatetime;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 /**
  *
  * @author crdzbird
  */
 public class InterfazPrincipal extends javax.swing.JFrame {
 
-    /**
-     * Creates new form InterfazPrincipal
-     */
+    private int opcion;
+    GraphicsDevice grafica = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    JSCAccordion accordion = new JSCAccordion();
+    JPanel panelMateriales = new JPanel();
+    JPanel panelProveedores = new JPanel();
+    JPanel panelInformacionGeneral = new JPanel();
+    JButton btnGestionarMateriales = new JButton();
+    JButton btnArbolMaterial = new JButton();
+    JButton btnVisualizarMateriales_Composicion = new JButton();
+    JButton btnGestionarProveedores = new JButton();
+    JButton btnVisualizarProveedores = new JButton();
+    JButton btnInformacionGeneral = new JButton();
+    JButton btnSalir = new JButton();
+    
+    public final void RellenarPaneles(){
+        //AGREGACION DE BORDERLAYOUT A LOS PANELES
+        panelMateriales.setLayout(new GridLayout(4, 1));
+        panelProveedores.setLayout(new GridLayout(3,1));
+        panelInformacionGeneral.setLayout(new GridLayout(2,1));
+        
+        btnGestionarMateriales.setText("Administrar Materiales");
+        btnArbolMaterial.setText("Arbol de Materiales");
+        btnVisualizarMateriales_Composicion.setText("Visualizar Composicion Mat.");
+        btnGestionarProveedores.setText("Administrar Proveedores");
+        btnVisualizarProveedores.setText("Visualizar Proveedores");
+        btnInformacionGeneral.setText("Datos de Desarrolladores");
+        
+        panelMateriales.add(btnGestionarMateriales);
+        panelMateriales.add(btnArbolMaterial);
+        panelMateriales.add(btnVisualizarMateriales_Composicion);
+        
+        panelProveedores.add(btnGestionarProveedores);
+        panelProveedores.add(btnVisualizarProveedores);
+        
+        panelInformacionGeneral.add(btnInformacionGeneral);
+    }
+    
     public InterfazPrincipal() {
         initComponents();
+        setLocationRelativeTo(null);
+        setExtendedState(MAXIMIZED_BOTH);
+        this.Usuario.setText(Usuario_Conectado.getUser());
+        RellenarPaneles();
+        this.jPanel3.setLayout(new BorderLayout());
+        this.jPanel3.add(MenuAcordion(), BorderLayout.CENTER);
+        this.jPanel3.add(AgregarBotonSalir(), BorderLayout.PAGE_END);
+        this.jDesktopPane1.setBorder(new ImagenFondoDesktop());
+        this.add(BarraEstado);
+    }
+    
+    private Component MenuAcordion() {
+        accordion.setTabOrientation(TabOrientation.VERTICAL);
+        agregarTabsProduccion(accordion);
+        add(accordion);
+        return accordion;
+    }
+    
+    private void agregarTabsProduccion(JSCAccordion accordion) {
+        panelMateriales.setOpaque(true);
+        panelMateriales.setBackground(Color.CYAN);
+        panelProveedores.setOpaque(true);
+        panelProveedores.setBackground(Color.CYAN);
+        panelInformacionGeneral.setOpaque(true);
+        panelInformacionGeneral.setBackground(Color.CYAN);
+
+        accordion.addTab("Administracion de Materiales", panelMateriales);
+        accordion.addTab("Administracion de Proveedores", panelProveedores);
+        accordion.addTab("Informacion del Sistema", panelInformacionGeneral);
+    }
+    
+    public void CerrarMDI(){
+        System.exit(0);
+    }
+    
+    private Component AgregarBotonSalir() {
+        btnSalir.setText("Salir del Sistema");
+        btnSalir.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                opcion = JOptionPane.showConfirmDialog(null, "Esta seguro que desea salir", "Advertencia", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+                if (opcion == 0) {
+                    CerrarMDI();
+                }
+            }
+        });
+        return btnSalir;
     }
 
     /**
@@ -27,21 +131,87 @@ public class InterfazPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        BarraEstado = new javax.swing.JPanel();
+        Usuario = new javax.swing.JLabel();
+        Fecha = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1154, Short.MAX_VALUE)
+        BarraEstado.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 1, true));
+
+        Usuario.setText("Usuario");
+
+        Fecha.setText("Fecha");
+
+        javax.swing.GroupLayout BarraEstadoLayout = new javax.swing.GroupLayout(BarraEstado);
+        BarraEstado.setLayout(BarraEstadoLayout);
+        BarraEstadoLayout.setHorizontalGroup(
+            BarraEstadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BarraEstadoLayout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(Usuario)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 454, Short.MAX_VALUE)
+                .addComponent(Fecha)
+                .addGap(33, 33, 33))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        BarraEstadoLayout.setVerticalGroup(
+            BarraEstadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BarraEstadoLayout.createSequentialGroup()
+                .addGap(5, 5, 5)
+                .addGroup(BarraEstadoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Usuario)
+                    .addComponent(Fecha))
+                .addGap(5, 5, 5))
+        );
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
+
+        jPanel1.setLayout(new java.awt.BorderLayout());
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 703, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(jPanel3, java.awt.BorderLayout.LINE_START);
+
+        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
+        jDesktopPane1.setLayout(jDesktopPane1Layout);
+        jDesktopPane1Layout.setHorizontalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1050, Short.MAX_VALUE)
+        );
+        jDesktopPane1Layout.setVerticalGroup(
+            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 707, Short.MAX_VALUE)
         );
 
+        jPanel1.add(jDesktopPane1, java.awt.BorderLayout.CENTER);
+
+        getContentPane().add(jPanel1);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        // TODO add your handling code here:
+        new Hora().start();
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
@@ -77,7 +247,33 @@ public class InterfazPrincipal extends javax.swing.JFrame {
             }
         });
     }
+    
+    class Hora extends Thread {
+
+        public void run() {
+            while (true) {
+                Date Hoy = new java.util.Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss a  EEEEEE, dd 'de' MMMM 'de' yyyy");
+                Fecha.setText(sdf.format(Hoy));
+                esperar();
+            }
+        }
+
+        public void esperar() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+            }
+        }
+
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel BarraEstado;
+    private javax.swing.JLabel Fecha;
+    private javax.swing.JLabel Usuario;
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
     // End of variables declaration//GEN-END:variables
 }
